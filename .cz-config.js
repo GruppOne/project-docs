196a97afc8cb36b94a6a3882e4fd51fe64b19c26
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-03-27 20:30:21 +0100

    chore: cambia commitizen adapter

---
+++ .cz-config.js
@@ -0,0 +1,73 @@
+const emptyScope = {
+  name: '               empty scope',
+  value: '',
+};
+
+module.exports = {
+  types: [
+    {
+      value: 'docs',
+      name: 'docs:       Documentation only changes',
+    },
+    {
+      value: 'fix',
+      name: 'fix:        A fix to LaTeX errors',
+    },
+    {
+      value: 'style',
+      name: 'style:      Changes that do not affect the meaning of the code (white-space, formatting, etc)',
+    },
+    {
+      value: 'refactor',
+      name: 'refactor:   A change that neither fixes a bug nor adds a feature',
+    },
+    {
+      value: 'build',
+      name: 'build:      Changes that affect the build system or external dependencies',
+    },
+    {
+      value: 'chore',
+      name: "chore:      Other changes that don't modify src or test files",
+    },
+    {
+      value: 'ci',
+      name: 'ci:         Changes to our CI configuration files and scripts',
+    },
+    {
+      value: 'revert',
+      name: 'revert:     Revert a previous commit',
+    },
+  ],
+
+  scopes: [
+    // this comments forces prettier to keep the array on multiple lines
+    emptyScope,
+    'verbali',
+    'AdR',
+    'glossario',
+    'PdP',
+    'PdQ',
+    'NdP',
+    'SdF',
+  ],
+
+  // it needs to match the value for field type. Eg.: 'fix'
+  scopeOverrides: {
+    build: [
+      // this comments forces prettier to keep the array on multiple lines
+      emptyScope,
+      {name: 'latex'},
+    ],
+    chore: [
+      // this comments forces prettier to keep the array on multiple lines
+      emptyScope,
+      {name: 'vscode'},
+    ],
+  },
+
+  allowBreakingChanges: [],
+  allowCustomScopes: false,
+  askForBreakingChangeFirst: true,
+  skipQuestions: ['breaking'],
+  subjectLimit: 72,
+};
